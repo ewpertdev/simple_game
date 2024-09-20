860f14863e017016d2302d334961a498ddbccfb6
ewpertdev <xblackcat93@gmail.com>
2024-09-20 16:44:25 +0200

    implementing checkYourself method

--- src/SimpleStartup.java
+++ src/SimpleStartup.java
@@ -1,4 +1,24 @@
 public class SimpleStartup {
+
+    public String checkYourself(int guess) {
+        String result ="miss";
+
+        for (int cell : locationCells) {
+            if (guess == cell ) {
+                result = "hit";
+
+                numOfHits++;
+
+                break;
+            }
+        }
+        if (numOfHits == locationCells.length) {
+            result = "kill";
+        }
+        System.out.println(result);
+        return result;
+    }
+
     public static void main(String[] args) {
         // instantiate a SimpleStartup object
         SimpleStartup dot = new SimpleStartup();
