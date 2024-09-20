8519a453b2fc52bffa33fe040c64da469e759fd1
ewpertdev <xblackcat93@gmail.com>
2024-09-20 16:49:17 +0200

    explanation to checkYourself method

--- src/SimpleStartup.java
+++ src/SimpleStartup.java
@@ -1,23 +1,32 @@
 public class SimpleStartup {
 
     public String checkYourself(int guess) {
+        // Make a variable to hold the result that we'll return. put "miss" in as the default cuz we assume the miss
         String result ="miss";
 
+        // repeat with each cell in the locationCells array (each cell location of the object)
         for (int cell : locationCells) {
+            // Compare the user guess to this element(cell) in the array
             if (guess == cell ) {
+                // we got a hit
                 result = "hit";
 
                 numOfHits++;
 
+                // get out of the loop, no need to test other cells
                 break;
-            }
-        }
+            } // end if
+        } // end for
+
+        // we're out of the loop, let's see if we're dead (hit 3 times) and change the result String to "kill"
         if (numOfHits == locationCells.length) {
             result = "kill";
         }
+        // Display the result for the user ("miss", unless it was changed to "hit" or "kill")
         System.out.println(result);
+        // Return the result back to the calling method
         return result;
-    }
+    } // end method
 
     public static void main(String[] args) {
         // instantiate a SimpleStartup object
