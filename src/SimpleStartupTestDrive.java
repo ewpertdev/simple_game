191ef3e8dba974c6ac36532fd2ec0f8003800640
ewpertdev <xblackcat93@gmail.com>
2024-09-21 12:34:42 +0200

    game class

--- src/SimpleStartupTestDrive.java
+++ src/SimpleStartupTestDrive.java
@@ -25,6 +25,32 @@ class SimpleStartup {
     }
 }
 
+class game {
+    public static void main(String[] args) {
+        int numOfGuesses = 0;
+        GameHelper helper = new GameHelper();
+
+        SimpleStartup theStartup = new SimpleStartup();
+
+        int randomNum = (int) (Math.random() * 5);
+
+        theStartup.setLocationCells(locations);
+
+        boolean isAlive = true;
+
+        while (isAlive) {
+            int guess = helper.getUserInput("enter a number");
+            String result = theStartup.checkYourself(guess);
+            numOfGuesses++;
+            if(result.equals("kill")) {
+                isAlive=false;
+                System.out.println("You took " + numOfGuesses+" guesses");
+            }
+        }
+    }
+
+}
+
 public class SimpleStartupTestDrive {
 
     public static void main(String[] args) {
@@ -41,6 +67,6 @@ public class SimpleStartupTestDrive {
         }
         System.out.println(testResult);
     }
-}
+
 
 
