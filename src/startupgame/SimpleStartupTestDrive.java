03a68b9b41099c2f9666a6d7f87fafc166dcc2c5
ewpertdev <xblackcat93@gmail.com>
2024-09-21 15:31:31 +0200

    use arraylist instead of regular array

--- src/startupgame/SimpleStartupTestDrive.java
+++ src/startupgame/SimpleStartupTestDrive.java
@@ -1,28 +1,26 @@
 package startupgame;
 
-class SimpleStartup {
-    private int[] locationCells;
-    private int numOfHits = 0;
+import java.util.ArrayList;
 
-    public void setLocationCells(int[] locs) {
+class Startup {
+    private ArrayList<String> locationCells;
+
+    public void setLocationCells(ArrayList<String> locs) {
         locationCells = locs;
     }
 
-    public String checkYourself(int guess) {
+    public String checkYourself(String userInput) {
         String result = "miss";
+        int index = locationCells.indexOf(userInput);
 
-        for (int cell : locationCells) {
-            if (guess == cell) {
-
+        if (index >= 0) {
+            locationCells.remove(index);
+            if (locationCells.isEmpty()) {
+                result = "kill";
+            } else {
                 result = "hit";
-                numOfHits++;
-                break;
             }
         }
-        if (numOfHits == locationCells.length) {
-            result = "kill";
-        }
-        System.out.println(result);
         return result;
     }
 }
